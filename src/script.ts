6683f929b6bc927eb58147b825fd8a5cbfb2f490
Julia <julia.k.tang@gatech.edu>
2025-12-27 16:24:30 -0500

    Files from tutorial

---
+++ src/script.ts
@@ -0,0 +1,35 @@
+import { redirectToAuthCodeFlow, getAccessToken } from "./authCodeWithPkce";
+
+const clientId = "3025f8b2eace4554a2e066bbed190e1a"
+const params = new URLSearchParams(window.location.search);
+const code = params.get("code");
+
+if (!code) {
+    redirectToAuthCodeFlow(clientId);
+} else {
+    const accessToken = await getAccessToken(clientId, code);
+    const profile = await fetchProfile(accessToken);
+    console.log(profile);
+    console.log(profile.display_name);
+    populateUI(profile);
+}
+
+async function fetchProfile(code: string): Promise<UserProfile> {
+    const result = await fetch("https://api.spotify.com/v1/me", {
+        method: "GET", headers: { Authorization: `Bearer ${code}` }
+    });
+
+    return await result.json();
+}
+
+function populateUI(profile: UserProfile) {
+    document.getElementById("displayName")!.innerText = profile.display_name;
+    document.getElementById("avatar")!.setAttribute("src", profile.images[0].url)
+    document.getElementById("id")!.innerText = profile.id;
+    document.getElementById("email")!.innerText = profile.email;
+    document.getElementById("uri")!.innerText = profile.uri;
+    document.getElementById("uri")!.setAttribute("href", profile.external_urls.spotify);
+    document.getElementById("url")!.innerText = profile.href;
+    document.getElementById("url")!.setAttribute("href", profile.href);
+    document.getElementById("imgUrl")!.innerText = profile.images[0].url;
+}
